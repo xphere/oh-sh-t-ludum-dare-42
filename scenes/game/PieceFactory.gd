1178bf644b21448355f94d88da164650b62ecd1d
Berny Cantos <be@rny.cc>
2018-08-24 02:56:52 +0200

    finetune random incoming pieces

--- scenes/game/PieceFactory.gd
+++ scenes/game/PieceFactory.gd
@@ -1,14 +1,44 @@
 extends Node
 
+class WeightedRand:
+	var weights = []
+	var total = 0
+
+
+	func add(weight):
+		weights.append(weight)
+		total += weight
+
+
+	func rand():
+		var rand = randi() % total
+		var index = 0
+		for weight in weights:
+			if rand < weight:
+				return index
+			index += 1
+			rand -= weight
+		return index
+
+
 const COLORS = [
 	Color(0.80, 0.00, 0.00, 1.00),
 	Color(0.00, 0.80, 0.00, 1.00),
 	Color(0.00, 0.00, 0.80, 1.00),
 ]
 
+var weights
+
+
+func _ready():
+	weights = WeightedRand.new()
+	for child in get_children():
+		var weight = int(child.name[-1]) # Weight is last character of name
+		weights.add(weight)
+
 
 func create_random(balloon):
-	var shape = randi() % get_child_count()
+	var shape = weights.rand()
 	var prototype = get_child(shape)
 	var clone = prototype.duplicate()
 	var color = randi() % 3
