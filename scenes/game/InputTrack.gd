ba95accee5a776e9b142434c1f76d70755f5bd7c
Berny Cantos <be@rny.cc>
2018-08-24 12:54:39 +0200

    add tween animation to balloons

--- scenes/game/InputTrack.gd
+++ scenes/game/InputTrack.gd
@@ -16,7 +16,29 @@ func _ready():
 
 func tick():
 	if $List.get_child_count() < MAX_ALLOWED:
-		create_random()
+		var balloon = create_random()
+		$Tween.interpolate_property(
+			balloon,
+			"rect_scale",
+			Vector2(0, 0),
+			Vector2(1, 1),
+			0.2,
+			Tween.TRANS_BACK,
+			Tween.EASE_OUT
+		)
+
+		balloon.modulate = Color(1.0, 1.0, 1.0, 0.0)
+		$Tween.interpolate_property(
+			balloon,
+			"modulate",
+			Color(1.0, 1.0, 1.0, 0.0),
+			Color(1.0, 1.0, 1.0, 1.0),
+			0.2,
+			Tween.TRANS_QUAD,
+			Tween.EASE_OUT
+		)
+
+		$Tween.start()
 
 
 func create_random():
@@ -24,8 +46,11 @@ func create_random():
 	var content = $Factory.create_random(balloon)
 
 	balloon.connect("gui_input", self, "_on_gui_input", [balloon])
+	balloon.rect_scale = Vector2(0, 0)
 	$List.add_child(balloon)
 	emit_signal("balloon_added", balloon)
+
+	return balloon
 
 
 func _on_gui_input(event, balloon):
