716a30077823cf929ec28c1d5d19673d794107d3
Berny Cantos <be@rny.cc>
2018-09-06 10:55:51 +0200

    create input packages from time to time

--- scenes/game/Cursor.gd
+++ scenes/game/Cursor.gd
@@ -1,8 +1,6 @@
 extends Position2D
 
-export(NodePath) var input_path
-
 
 func _input(event):
-	if input_path and Input.is_action_just_pressed("ui_accept"):
-		get_node(input_path).add_package(preload("res://scenes/game/Package.tscn").instance())
+	if event is InputEventMouseMotion:
+		position = event.position
