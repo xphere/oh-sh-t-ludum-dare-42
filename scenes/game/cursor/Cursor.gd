71d0979ca9bf91f21723826fdee60d85aa39483c
Berny Cantos <be@rny.cc>
2018-09-07 13:33:31 +0200

    move events to signals

--- scenes/game/cursor/Cursor.gd
+++ scenes/game/cursor/Cursor.gd
@@ -1,5 +1,11 @@
 extends Area2D
 
+signal click(selected_element)
+signal right_click(selected_element)
+signal enter(element)
+signal leave(element)
+
+
 var selection_stack = []
 var current_selection
 
@@ -10,29 +16,48 @@ func _input(event):
 
 	if event is InputEventMouseButton and event.is_pressed():
 		match event.button_index:
-			BUTTON_LEFT: event("click", current_selection)
-			BUTTON_RIGHT: event("right_click", current_selection)
-
-
-func event(event_name, context = null):
-	$States.event(event_name, context)
+			BUTTON_LEFT: emit_signal("click", current_selection)
+			BUTTON_RIGHT: emit_signal("right_click", current_selection)
 
 
 func when_entered(area):
 	area = target_of(area)
 	if current_selection:
 		selection_stack.push_back(current_selection)
+		emit_signal("leave", current_selection)
 	current_selection = area
-	event("enter", area)
+	emit_signal("enter", area)
 
 
 func when_exited(area):
 	area = target_of(area)
-	if current_selection == area:
-		current_selection = null if selection_stack.empty() else selection_stack.pop_back()
-	else:
+	emit_signal("leave", area)
+
+	if current_selection != area:
 		selection_stack.erase(area)
-	event("leave", area)
+		return
+
+	if selection_stack.empty():
+		current_selection = null
+	else:
+		current_selection = selection_stack.pop_back()
+		emit_signal("enter", current_selection)
+
+
+func on_event_click(element):
+	$States.event("click", element)
+
+
+func on_event_right_click(element):
+	$States.event("right_click", element)
+
+
+func on_event_enter(element):
+	$States.event("enter", element)
+
+
+func on_event_leave(element):
+	$States.event("leave", element)
 
 
 func target_of(element):
@@ -43,4 +68,3 @@ func target_of(element):
 		element = element.get_parent()
 		if element.is_in_group("target"):
 			return element
-
