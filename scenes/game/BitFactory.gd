8ce680fb5e3820f947161ffd6589ce7a7ef8c777
Berny Cantos <be@rny.cc>
2018-09-07 19:22:11 +0200

    show package tooltip on hover

--- scenes/game/BitFactory.gd
+++ scenes/game/BitFactory.gd
@@ -1,12 +1,89 @@
 extends Node2D
 
-func create(index, color):
-	var clone = get_child(index).duplicate()
+enum PieceColor {
+	RED = 1,
+	BLUE = 2,
+	GREEN = 4,
+	WHITE = 8,
+	BLACK = 16,
+}
 
-	clone.visible = true
-	clone.set_meta("bit_index", index)
-	clone.set_meta("bit_color", color)
-	for child in clone.get_children():
-		child.region_rect.position.y = color * 7
+export(int, FLAGS, "Red", "Blue", "Green", "White", "Black") var available_colors
+export(bool) var mix_colors = false
 
-	return clone
+var possible_pieces = [
+	{
+		"shape": [
+			"x",
+		],
+	},
+	{
+		"shape": [
+			"x ",
+			"xx",
+		],
+	},
+	{
+		"shape": [
+			"xx",
+			"xx",
+		],
+	},
+	{
+		"shape": [
+			"xx",
+			" xx",
+		],
+	},
+	{
+		"shape": [
+			" xx",
+			"xx",
+		],
+	},
+	{
+		"shape": [
+			"x  ",
+			"xxx",
+		],
+	},
+	{
+		"shape": [
+			"  x",
+			"xxx",
+		],
+	},
+]
+
+func _ready():
+	randomize()
+	for piece in possible_pieces:
+		var positions = []
+		var position = Vector2(0, 0)
+		var max_size = Vector2(0, 0)
+		for row in piece.shape:
+			max_size = Vector2(
+				max(max_size.x, row.length()),
+				max(max_size.y, position.y)
+			)
+			for column in row:
+				if column != " ":
+					positions.push_back(position)
+				position.x += 1
+			position = Vector2(0, position.y + 1)
+		piece.positions = positions
+		piece.center = max_size / 2.0 - Vector2(0.5, 0.5)
+
+
+func create():
+	var content = $Content.duplicate()
+
+	var piece = possible_pieces[randi() % possible_pieces.size()]
+	var tooltip = content.get_node("Tooltip/Pieces")
+	tooltip.position = -piece.center
+	for position in piece.positions:
+		var bit = $Pieces/Red/Placeholder.duplicate()
+		bit.offset = position
+		tooltip.add_child(bit)
+
+	return content
--- scenes/game/package/Package.gd
+++ scenes/game/package/Package.gd
@@ -2,7 +2,24 @@ extends RigidBody2D
 
 var entered = false
 var selected = false
-var state = false
+var state = null
+var content = null
+var tooltip = null
+var placeholder = null
+
+
+func _ready():
+	update_state()
+
+
+func from_content(new_content):
+	content = new_content
+
+	if content.has_node("Tooltip"):
+		tooltip = content.get_node("Tooltip").duplicate()
+		$Pivot/Tooltip/Balloon/Container.add_child(tooltip)
+	else:
+		tooltip = null
 
 
 func on_enter():
@@ -26,6 +43,9 @@ func update_state():
 		return
 
 	state = next_state
+
+	$Pivot/Tooltip.visible = state and tooltip != null
+
 	$Pivot/Border.visible = state
 	if state and not $AnimationPlayer.is_playing():
 		$AnimationPlayer.play("Selected")
