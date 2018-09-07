c66449938f551251a91997d5e5ce096854a9a038
Berny Cantos <be@rny.cc>
2018-09-07 20:32:09 +0200

    add colors to packages

--- scenes/game/BitFactory.gd
+++ scenes/game/BitFactory.gd
@@ -6,11 +6,13 @@ enum PieceColor {
 	GREEN = 4,
 	WHITE = 8,
 	BLACK = 16,
+	MAX = 32,
 }
 
 export(int, FLAGS, "Red", "Blue", "Green", "White", "Black") var available_colors
-export(bool) var mix_colors = false
+export(float, 0.0, 1.0) var mixed_colors_probability = 0.0
 
+var colors = []
 var possible_pieces = [
 	{
 		"shape": [
@@ -57,6 +59,18 @@ var possible_pieces = [
 
 func _ready():
 	randomize()
+
+	var all_colors = {
+		PieceColor.RED: $Pieces/Red/Placeholder,
+		PieceColor.BLUE: $Pieces/Blue/Placeholder,
+		PieceColor.GREEN: $Pieces/Green/Placeholder,
+		PieceColor.WHITE: $Pieces/White/Placeholder,
+		PieceColor.BLACK: $Pieces/Black/Placeholder,
+	}
+	for color in all_colors.keys():
+		if available_colors & color:
+			colors.push_back(all_colors[color])
+
 	for piece in possible_pieces:
 		var positions = []
 		var position = Vector2(0, 0)
@@ -72,18 +86,27 @@ func _ready():
 				position.x += 1
 			position = Vector2(0, position.y + 1)
 		piece.positions = positions
-		piece.center = max_size / 2.0 - Vector2(0.5, 0.5)
+		piece.center = $Content/Tooltip/Pieces.scale * (max_size - Vector2(1.0, 1.0)) / 2.0
 
 
 func create():
 	var content = $Content.duplicate()
 
+	var color = random_color()
 	var piece = possible_pieces[randi() % possible_pieces.size()]
 	var tooltip = content.get_node("Tooltip/Pieces")
 	tooltip.position = -piece.center
+
+	var use_mixed_colors = randf() < mixed_colors_probability
 	for position in piece.positions:
-		var bit = $Pieces/Red/Placeholder.duplicate()
+		var bit = color.duplicate()
 		bit.offset = position
 		tooltip.add_child(bit)
+		if use_mixed_colors:
+			color = random_color()
 
 	return content
+
+
+func random_color():
+	return colors[randi() % colors.size()]
